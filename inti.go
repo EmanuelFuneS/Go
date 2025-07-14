acdc2e3e35bfc606af4a9d4c7679e5e643970db0
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-07-14 16:50:36 -0300

    create module and use print fmt method

--- inti.go
+++ inti.go
@@ -1,5 +1,8 @@
 package main //punto de entrada
 
+import "fmt" // paquete de formateo
+
 func main() { //funcion principal
+	fmt.Print("print con el modulo fmt \n")
 	print("Hola Mundo! con Go")
 }
