097b6f9b0a2ddf9b6d8ffcdfd3e4f7ade228ee44
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2025-07-14 17:07:25 -0300

    some types and prints types

--- inti.go
+++ inti.go
@@ -1,8 +1,41 @@
 package main //punto de entrada
 
-import "fmt" // paquete de formateo
+import (
+	"fmt" // paquete de formateo
+	"reflect"
+)
+
+//comentario simple
+/*
+Comentarios de doble lineas
+*/
 
 func main() { //funcion principal
-	fmt.Print("print con el modulo fmt \n")
-	print("Hola Mundo! con Go")
+	fmt.Print("print con el modulo fmt \n") //print sobre la misma linea
+	fmt.Println("print con salto de linea")
+	print("Hola Mundo! con Go \n")
+
+	//Variables
+
+	var myString string = "My String \n"
+	fmt.Print(myString)
+
+	myString = "Other String \n"
+
+	fmt.Print(myString)
+
+	// Tipos de datos
+
+	var name string = "Emanuel"
+	fmt.Println(name)
+
+	var age int32 = 29 //multiples tipos de int, int32, int64, etc
+	fmt.Println(age)
+
+	fmt.Printf("%s %d", name, age) //Print con formate, Println lo puede manejar
+	fmt.Println(name, age)
+
+	//Mostrar tipos de datos
+	fmt.Println(reflect.TypeOf(age))
+	fmt.Println(reflect.TypeOf(name))
 }
